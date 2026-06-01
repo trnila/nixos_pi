8e37b01edc7c3b3410fb152b7a0e61acf7a72366
Daniel Trnka <daniel.trnka@gmail.com>
2026-06-01 17:31:03 +0200

    ycx

--- flake.nix
+++ flake.nix
@@ -1,14 +1,19 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.nextbike.url = "github:trnila/nextbike_rides_viewer/nix";
-  inputs.deploy-rs.url = "github:serokell/deploy-rs";
   outputs =
     {
       self,
       nixpkgs,
       nextbike,
-      deploy-rs,
     }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
     {
       nixosConfigurations.picko = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
@@ -28,19 +33,20 @@
 
         ];
       };
-
       sdImage = self.nixosConfigurations.picko.config.system.build.sdImage;
 
-      deploy.nodes.pi = {
-        #hostname = "pi.trnila.eu";
-        hostname = "nix";
-        profiles.system = {
-          user = "root";
-          sshUser = "root";
-          path = deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.picko;
-        };
-      };
-
-      checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.nixos-rebuild
+            ];
+          };
+        }
+      );
     };
 }
