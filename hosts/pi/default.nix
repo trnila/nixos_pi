7789c3e6a99008f13a9e47861d491b68bf460365
Daniel Trnka <daniel.trnka@gmail.com>
2026-07-27 13:27:43 +0200

    octoprint: split into module

--- hosts/pi/default.nix
+++ hosts/pi/default.nix
@@ -7,51 +7,8 @@
 }:
 
 {
-  nixpkgs.overlays = [
-    (final: prev: {
-      octoprint = prev.octoprint.override {
-        packageOverrides = self: super: {
-          octoprint = super.octoprint.overridePythonAttrs (oldAttrs: {
-            propagatedBuildInputs = (lib.remove self.class-doc (oldAttrs.propagatedBuildInputs or [ ])) ++ [
-              self.more-itertools
-            ];
-          });
-
-          octoprint-filecheck = super.octoprint-filecheck.overridePythonAttrs (oldAttrs: {
-            version = "2025.7.23";
-
-            src = final.fetchFromGitHub {
-              owner = "OctoPrint";
-              repo = "OctoPrint-FileCheck";
-              rev = "2025.7.23";
-              hash = "sha256-Y3JVfbe+bZz2t65OqdjvVVqTSa0VUPoCaxvE+zQ+Qts=";
-            };
-          });
-
-          octoprint-firmwarecheck = super.octoprint-firmwarecheck.overridePythonAttrs (oldAttrs: {
-            version = "2025.7.23";
-
-            src = final.fetchFromGitHub {
-              owner = "OctoPrint";
-              repo = "OctoPrint-FirmwareCheck";
-              rev = "2025.7.23";
-              hash = "sha256-QPchpyeotB5IKbfES74CJlhw3sz8Q1df/+n5dpbrHSs=";
-            };
-          });
-
-          octoprint-pisupport = super.octoprint-pisupport.overridePythonAttrs (oldAttrs: {
-            version = "2025.7.23";
-
-            src = final.fetchFromGitHub {
-              owner = "OctoPrint";
-              repo = "OctoPrint-PiSupport";
-              rev = "2025.7.23";
-              hash = "sha256-bXjRGxIwi+UnVts2HO9viOJqa2AmZ/CL7wuoyzRbAEw=";
-            };
-          });
-        };
-      };
-    })
+  imports = [
+    ./octoprint.nix
   ];
 
   system.stateVersion = "26.05";
@@ -134,7 +91,6 @@
       ];
   };
   services.thelounge.enable = true;
-  services.octoprint.enable = true;
   services.redis.enable = true;
   services.nextbike-rides-viewer.enable = true;
   services.traefik = {
@@ -177,11 +133,6 @@
             middlewares = [ "to-github" ];
             service = "noop@internal";
           };
-          printer = {
-            rule = "Host(`3dprinter.trnila.eu`)";
-            entryPoints = [ "https" ];
-            service = "octoprint";
-          };
           hass = {
             rule = "Host(`hass.trnila.eu`)";
             entryPoints = [ "https" ];
@@ -202,14 +153,6 @@
         };
 
         services = {
-          octoprint = {
-            loadBalancer = {
-              servers = [
-                { url = "http://localhost:5000"; }
-              ];
-            };
-          };
-
           hass = {
             loadBalancer = {
               servers = [
