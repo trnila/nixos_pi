66a3055082645fcd820cb7789d1dc6011cbff9a4
Daniel Trnka <daniel.trnka@gmail.com>
2026-07-30 21:08:15 +0200

    deploy.sh: deploy system based on hostname

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
-DEPLOY_HOST=${1:-pi}
-nixos-rebuild --flake .#pi --target-host "$DEPLOY_HOST" --build-host "$DEPLOY_HOST" --no-reexec switch
+DEPLOY_HOST=${1}
+
+if [ -z "$DEPLOY_HOST" ]; then
+  echo "Usage: $0 <deploy-host>"
+  exit 1
+fi
+
+nixos-rebuild --flake ".#$DEPLOY_HOST" --target-host "root@$DEPLOY_HOST" --build-host "root@$DEPLOY_HOST" --no-reexec switch
