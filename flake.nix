e4cd508ae42a6739399a4685ade3bdaf68bc55c6
Daniel Trnka <daniel.trnka@gmail.com>
2026-09-13 09:12:12 +0200

    nixos switch to master

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
   inputs.nextbike.url = "github:trnila/nextbike_rides_viewer";
   inputs.assistant.url = "github:trnila/assistant";
 
